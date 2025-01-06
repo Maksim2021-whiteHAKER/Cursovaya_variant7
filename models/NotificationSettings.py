620acda618dc73bd51eda302c368970e18dad9f5
Maksim2021-whiteHAKER <maksim25082002x5@gmail.com>
2025-01-06 20:13:38 +0400

    add .gitignore, m signout, Notif

--- models/NotificationSettings.py
+++ models/NotificationSettings.py
@@ -1,4 +1,4 @@
-from sqlalchemy import DateTime, Column, ForeignKey, Integer, String
+from sqlalchemy import DateTime, Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 from app_data.definitions import Base
 
@@ -7,8 +7,8 @@ class NotificationSettings(Base):
     __tablename__ = 'notification_settings'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey=('user.id'))
-    device_id = Column(Integer, ForeignKey=('device.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    device_id = Column(Integer, ForeignKey('device.id'))
     email = Column(String, nullable=True)
     phone = Column(String, nullable=True)
-    critical_value = Column(float, nullable=True)
+    critical_value = Column(Float, nullable=True)
